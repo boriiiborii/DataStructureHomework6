b9a0125b1b9233051bcaf345353ff9fe39961eb3
Haena <ghkwlsspdl@naver.com>
2024-04-12 21:50:25 +0900

    프로젝트 생성 및 초기 프린트 세팅

---
+++ circularQ.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main() {
+    printf("----------------------------------------------\n");
+    printf("                 Circular Q                   \n");
+    printf("----------------------------------------------\n");
+    printf("Insert=i, Delete=d, PtrintQ=p, Debug=b, Quit=q\n");
+    printf("----------------------------------------------\n");
+    printf("Command = ");
+}
